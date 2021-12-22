5c28be4266a00ac94b78dbb68d0ba0859986776d
farzanapomy <farzanapomy56@gmail.com>
2021-12-23 04:05:21 +0600

    updated

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 require('dotenv').config();
-
 const app = express()
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
 const port = process.env.PORT || 5000
 
 
@@ -28,7 +29,7 @@ async function run() {
         console.log('databases connected');
 
 
-        // post services and get service
+        // post services and get service and single service
         app.post('/services', async (req, res) => {
             const service = req.body;
             const result = await serviceCollection.insertOne(service)
@@ -40,6 +41,15 @@ async function run() {
             const cursor = serviceCollection.find({});
             const service = await cursor.toArray();
             res.send(service);
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id);
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query)
+            console.log(service);
+            res.json(service)
         })
 
 
