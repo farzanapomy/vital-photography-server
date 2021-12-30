133421d15b5fc4e150646829e5123863e65434c1
farzanapomy <farzanapomy56@gmail.com>
2021-12-30 08:16:41 +0600

    add

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ async function run() {
 
         // payment system 
 
-        app.get('/allOrders/:id', async (req, res) => {
+        app.get('/payment/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await orderCollection.findOne(query)
